01b9b0c48b875579ac21d3e8c51dd6d0a4fcadec
Povpanha28 <sengkim28140@gmail.com>
2025-02-09 18:29:50 +0700

    finished function

---
+++ Ex5/app.js
@@ -0,0 +1,72 @@
+const taskInput = document.getElementById('taskInput');
+const prioritySelect = document.getElementById('prioritySelect');
+const addTaskBtn = document.getElementById('addTask');
+const allbtn = document.getElementById('all');
+const displayTask = document.getElementById('displayTask');
+const highbtn = document.getElementById('high');
+const lowbtn = document.getElementById('low');
+
+
+let tasks = [
+    {
+        description: '',
+        priority: 0,
+    },];
+    addTaskBtn.addEventListener('click', function () {
+        const newTask = {
+            description: taskInput.value,
+            priority: parseInt(prioritySelect.value), // Convert to number
+        };
+    
+        tasks.push(newTask); // Add new task to the array
+    
+        displayTask.innerHTML = ''; // Clear previous tasks
+    
+        // Re-render all tasks
+        tasks.forEach(task => {
+            const taskDiv = document.createElement('div');
+            taskDiv.innerHTML = `<h3>${task.description}</h3>`;
+            taskDiv.style.backgroundColor = task.priority === 1 ? 'red' : 'gray';
+            displayTask.appendChild(taskDiv);
+        });
+        taskInput.value = ''; // Clear input
+    });
+    
+
+allbtn.addEventListener('click', function () {
+    displayTask.innerHTML = ''; // Clear previous tasks
+    tasks.forEach(task => {
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `<h3>${task.description}</h3>`;
+        newDiv.style.backgroundColor = task.priority === 1 ? 'red' : 'gray';
+        displayTask.appendChild(newDiv);
+    });
+});
+
+highbtn.addEventListener('click', function () {
+    displayTask.innerHTML = ''; // Clear previous tasks
+    tasks.forEach(task => {
+        if (task.priority === 1) {
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `<h3>${task.description}</h3>`;
+            newDiv.style.backgroundColor = task.priority === 1 ? 'red' : 'gray';
+            displayTask.appendChild(newDiv);
+        }
+    });
+})
+
+lowbtn.addEventListener('click', function () {
+    displayTask.innerHTML = ''; // Clear previous tasks
+    tasks.forEach(task => {
+        if (task.priority === 0) {
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `<h3>${task.description}</h3>`;
+            newDiv.style.backgroundColor = task.priority === 1 ? 'red' : 'gray';
+            displayTask.appendChild(newDiv);
+        }
+    });
+}) ;
+
+
+
+
